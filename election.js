2dec6b41ef83fd4bdd1e4db97ed06e8e63f6b8c4
vikrep <protsenko.viktor@gmail.com>
2018-03-07 19:14:48 +0000

    exersise 1

--- election.js
+++ election.js
@@ -6,8 +6,12 @@
  * 1 - Convert candidates object to array
  */
 function candidatesObjToArray(candidates) {
-
-}
+    var arrayCandidates = Object.keys(candidates).map(function (key) {  // first method
+        return candidates[key];
+    });
+    // var arrayCandidates = Object.values(candidates);   // second method
+    return arrayCandidates;
+};
 
 /**
  * 2 - Remove any voters who have voted for more than 2 people, or have voted for the same person twice.
@@ -43,20 +47,20 @@ function winnerMessage(winner) {
 
 // A sample population of a small number of voters, stored as an array
 let votingPopulation = [
-    {name: 'Jane Finnegan', age: 19, votingCard: [1,3]},
-    {name: 'Norman Beracha', age: 35, votingCard: [3,4]},
-    {name: 'Salome Kadek', age: 22, votingCard: [2,1,3]},
-    {name: 'Wei Li', age: 19, votingCard: [1,2]},
-    {name: 'Sam MacKinnon', age: 59, votingCard: [1,4]}
+    { name: 'Jane Finnegan', age: 19, votingCard: [1, 3] },
+    { name: 'Norman Beracha', age: 35, votingCard: [3, 4] },
+    { name: 'Salome Kadek', age: 22, votingCard: [2, 1, 3] },
+    { name: 'Wei Li', age: 19, votingCard: [1, 2] },
+    { name: 'Sam MacKinnon', age: 59, votingCard: [1, 4] }
 ];
 
 // The election candidates, stored as an object where each object key is the candidate ID, and the object
 // value is the candidate object itself.
 let candidates = {
-    1: {name: 'Tamara Faiza', age: 46, votingCard: [1,1], party: 'Pizza Party', numVotes: 0},
-    2: {name: 'Aylin Duke', age: 39, votingCard: [2,2], party: 'Foam Party', numVotes: 0},
-    3: {name: 'Clay Roderick', age: 54, votingCard: [3,4], party: 'Flat Earth Party', numVotes: 0},
-    4: {name: 'Nour al-Din', age: 32, votingCard: [4,1], party: 'Pizza Party', numVotes: 0}
+    1: { name: 'Tamara Faiza', age: 46, votingCard: [1, 1], party: 'Pizza Party', numVotes: 0 },
+    2: { name: 'Aylin Duke', age: 39, votingCard: [2, 2], party: 'Foam Party', numVotes: 0 },
+    3: { name: 'Clay Roderick', age: 54, votingCard: [3, 4], party: 'Flat Earth Party', numVotes: 0 },
+    4: { name: 'Nour al-Din', age: 32, votingCard: [4, 1], party: 'Pizza Party', numVotes: 0 }
 };
 
 let allVoters = votingPopulation.concat(candidatesObjToArray(candidates));
@@ -68,10 +72,10 @@ candidates = runElection(validVoters, candidates);
 let winner = getWinner(candidates);
 
 module.exports = {
-  candidatesObjToArray,
-  filterInvalidVoters,
-  runElection,
-  getWinner,
-  winnerMessage
+    candidatesObjToArray,
+    filterInvalidVoters,
+    runElection,
+    getWinner,
+    winnerMessage
 }
 
